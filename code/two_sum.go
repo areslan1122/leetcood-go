6e93941e1a15f90c3c974719f8431276fa0ce381
A Reslan <areslan@areslan.local>
2019-03-20 17:00:39 +0800

    [finish] Two Sum

---
+++ code/two_sum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j <= len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+
+	fmt.Println(twoSum([]int{2, 11, 7, 15}, 9))
+}
