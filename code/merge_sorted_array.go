1a77b104f57688f29fe2d8896664093907fc0669
areslan <areslan@areslan-mac.lan>
2020-02-11 14:46:55 +0800

    Merge Sorted Array, time O(n*n) -> O(n)

--- code/merge_sorted_array.go
+++ code/merge_sorted_array.go
@@ -1,19 +1,38 @@
 package main
 
+//func merge(nums1 []int, m int, nums2 []int, n int) {
+//
+//	for i := 0; i < n; i++ {
+//		nums1[m+i] = nums2[i]
+//	}
+//
+//	for i := 0; i < m+n; i++ {
+//		for j := i; j < m+n; j++ {
+//			if nums1[i] > nums1[j] {
+//				tmp := nums1[i]
+//				nums1[i] = nums1[j]
+//				nums1[j] = tmp
+//			}
+//		}
+//	}
+//
+//}
+
 func merge(nums1 []int, m int, nums2 []int, n int) {
 
-	for i := 0; i < n; i++ {
-		nums1[m+i] = nums2[i]
-	}
-
-	for i := 0; i < m+n; i++ {
-		for j := i; j < m+n; j++ {
-			if nums1[i] > nums1[j] {
-				tmp := nums1[i]
-				nums1[i] = nums1[j]
-				nums1[j] = tmp
-			}
+	for m > 0 && n > 0 {
+		if nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
 		}
+
 	}
 
+	for n > 0 {
+		nums1[n-1] = nums2[n-1]
+		n--
+	}
 }
