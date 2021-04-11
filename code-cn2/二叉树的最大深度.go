031fe7d3448a14c67caca4f08739b27c674c6c72
areslan <areslan1122@gmail.com>
2021-04-11 17:17:20 +0800

    leetcode update 二叉树的最大深度

--- code-cn2/二叉树的最大深度.go
+++ code-cn2/二叉树的最大深度.go
@@ -9,23 +9,17 @@ package main
  * }
  */
 func maxDepth(root *TreeNode) int {
-	ans := 0
-	ans = maxDepthHelper(root, ans)
-	return ans
-}
-
-func maxDepthHelper(root *TreeNode, depth int) int {
 
 	if root == nil {
-		return depth
+		return 0
 	}
-	left := maxDepthHelper(root.Left, depth+1)
-	right := maxDepthHelper(root.Right, depth+1)
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
 
 	if left > right {
-		depth = left
+		return left + 1
 	} else {
-		depth = right
+		return right + 1
 	}
-	return depth
 }
